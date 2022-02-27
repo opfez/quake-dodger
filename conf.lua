a9856794ee44fbc5b2890588e2734e383b957cb4
opfez <opfez@disroot.org>
2022-02-27 11:42:09 +0100

    [erik] add vector lib

--- conf.lua
+++ conf.lua
@@ -4,6 +4,9 @@ WHEIGHT = 640
 WIDTH = WWIDTH / SCALE
 HEIGHT = WHEIGHT / SCALE
 
+require("lib/functional")
+vector = require("lib/vector")
+
 function love.conf(t)
     t.window.title = "Love jam 2022"
     t.window.width = WWIDTH
